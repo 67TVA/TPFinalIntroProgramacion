0aa549523aaafbaf656fc4a72ccef4155062c896
67TVA <939mail2@gmail.com>
2025-02-23 17:55:43 -0300

    Update final main actualizable.cpp

--- main actualizable.cpp
+++ main actualizable.cpp
@@ -12,28 +12,7 @@ const int bordeIzq = 2;
 const int bordeDer = 24;
 const int bordeInf = 14;
 
-class Sistema{
-private:
-	int puntaje = 0;
-	int vidas = 5;
-	int mensaje;
-	bool DisparoActivo = false;
-public:
-	void DibujarPlantilla();
-};
 
-void Sistema::DibujarPlantilla(){
-	cout<<"Instrucciones:"<<endl;
-	cout<<"Movimiento: WASD"<<endl;
-	cout<<"Muevete para esquivar los meteoritos y dispara a las X!(25)"<<endl;
-	cout<<"-------------------------"<<endl;
-	for(int i = 0;i < 10;i++){
-	cout<<"-                       -"<<endl;};
-	cout<<"-------------------------"<<endl;
-	cout<<"                         "<<endl;
-	cout<<"> Vidas: "<<vidas<<endl;
-	cout<<"> Puntaje: "<<puntaje<<endl;
-}
 
 class EstructuraDePosicion{
 private:
@@ -202,113 +181,167 @@ void Disparo::mover(){
 	setY (PosY() - 1);
 	
 }	
-
-
-
-
-int main(int argc, char *argv[]) {
-	int vidas = 5;
+class Sistema{
+private:
 	int puntaje = 0;
+	int vidas = 5;
 	int mensaje;
 	bool DisparoActivo = false;
-    Sistema sistema;
-	sistema.DibujarPlantilla();
-	NaveJugador NaveJ;
-	NaveJ.resetposicion();
 	EstructuraDePosicion *p1 = new X(20,31);
 	EstructuraDePosicion *p2 = new X(30,32);
-	p2->setY(7);
 	EstructuraDePosicion *m1 = new Meteorito;
 	EstructuraDePosicion *d1 = new Disparo;
+	NaveJugador NaveJ;
 	
-	
-	while(vidas > 0){
-		p1->start();
-		p2->start();
-		m1->start();
-		if (m1->PosX() == NaveJ.X && m1->PosY() == NaveJ.Y){
-			NaveJ.resetposicion();
-			vidas--;
-			gotoxy(10,17);
-			textcolor(7);
-			cout<<vidas;
-		};
-		if (DisparoActivo){
-			d1->start();
-			if (d1->PosY() == bordeSup-1){
-				DisparoActivo = false;
-				gotoxy(d1->PosX(), bordeSup-1);
-				textcolor(7);
-				cout<<"-";
-			}
-			if(d1->PosX() == p1->PosX() && d1->PosY() == p1->PosY() || d1->PosX() == p2->PosX() && d1->PosY() == p2->PosY()){
-				puntaje += 100;
-				DisparoActivo = false;
-				gotoxy (27,10);
-				mensaje = rand()%3+1;
-				switch(mensaje){
-				case 1:
-					cout<<"Good shot!        ";
-					break;
-				case 2:
-					cout<<"Bullseye!         ";
-					break;
-				case 3:
-					cout<<"Amazing!         ";
-					break;
-					
-				};
-				gotoxy(12,18);
-				textcolor(7);
-			    cout<<puntaje;
-			};
-			
-		};
+public:
+	Sistema();
+	void DibujarPlantilla();
+	void Teclado();
+	void gameover();
+	void Logica();
+	void Colisiones();
+	void IniciarJuego();
+};
 
-		if (kbhit()) {
-			
-			int tecla= getch();
-			switch(tecla){
-			case 'w':
-				if (NaveJ.Y > bordeSup){
-					NaveJ.borrar();
-					NaveJ.Y -= 1;
-					NaveJ.dibujar();
-				};
-                break;
-			case 'a':
-				if (NaveJ.X > bordeIzq){
-					NaveJ.borrar();
-					NaveJ.X -= 1;
-					NaveJ.dibujar();
-				};
-				break;
-			case 's':
-				if (NaveJ.Y < bordeInf){
-					NaveJ.borrar();
-					NaveJ.Y += 1;
-					NaveJ.dibujar();
-				};
-				break;
-			case 'd':
-				if (NaveJ.X < bordeDer) {
-					NaveJ.borrar();
-					NaveJ.X += 1;
-					NaveJ.dibujar();
-				};
-				break;
-			case ' ':
-				if (!DisparoActivo){
-					d1->setX(NaveJ.X);
-					d1->setY(NaveJ.Y-1);
-					DisparoActivo = true;
-				}
-				break;
-			};
-			
-		};
-	};
+
+	Sistema::Sistema(){
+	NaveJ.resetposicion();
+	p2->setY(7);
+	}
 	
+void Sistema::DibujarPlantilla(){
+    gotoxy(1,1);
+	
+	cout<<"Instrucciones:"<<endl;
+	cout<<"Movimiento: WASD"<<endl;
+	cout<<"Muevete para esquivar los meteoritos y dispara a las X!"<<endl;
+	cout<<"-------------------------"<<endl;
+	for(int i = 0;i < 10;i++){
+		cout<<"-                       -"<<endl;};
+	cout<<"-------------------------"<<endl;
+	cout<<"                         "<<endl;
+	cout<<"> Vidas: "<<vidas<<endl;
+	cout<<"> Puntaje: "<<puntaje<<endl;
+	NaveJ.resetposicion();
+}
+
+
+
+void Sistema::Teclado(){
+	if (kbhit()) {
+		
+		int tecla= getch();
+		switch(tecla){
+		case 'w':
+			if (NaveJ.Y > bordeSup){
+				NaveJ.borrar();
+				NaveJ.Y -= 1;
+				NaveJ.dibujar();
+			};
+			break;
+		case 'a':
+			if (NaveJ.X > bordeIzq){
+				NaveJ.borrar();
+				NaveJ.X -= 1;
+				NaveJ.dibujar();
+			};
+			break;
+		case 's':
+			if (NaveJ.Y < bordeInf){
+				NaveJ.borrar();
+				NaveJ.Y += 1;
+				NaveJ.dibujar();
+			};
+			break;
+		case 'd':
+			if (NaveJ.X < bordeDer) {
+				NaveJ.borrar();
+				NaveJ.X += 1;
+				NaveJ.dibujar();
+			};
+			break;
+		case ' ':
+			if (!DisparoActivo){
+				d1->setX(NaveJ.X);
+				d1->setY(NaveJ.Y-1);
+				DisparoActivo = true;
+			}
+			break;
+		};
+		
+	};
+}
+
+
+void Sistema::gameover(){
+	gotoxy(9,9);
+	textcolor(4);
+	cout<<"GAMEOVER";
+	gotoxy(9,10);
+	cout<<"Puntaje: "<<puntaje;
+	textcolor(7);
+	gotoxy(1,17);
+}
+void Sistema::Logica(){
+	p1->start();
+	p2->start();
+	m1->start();
+}
+void Sistema::Colisiones(){
+	if (m1->PosX() == NaveJ.X && m1->PosY() == NaveJ.Y){
+		NaveJ.resetposicion();
+		vidas--;
+		gotoxy(10,17);
+		textcolor(7);
+		cout<<vidas;
+	};
+	if (DisparoActivo){
+		d1->start();
+		if (d1->PosY() == bordeSup-1){
+			DisparoActivo = false;
+			gotoxy(d1->PosX(), bordeSup-1);
+			textcolor(7);
+			cout<<"-";
+		}
+		if(d1->PosX() == p1->PosX() && d1->PosY() == p1->PosY() || d1->PosX() == p2->PosX() && d1->PosY() == p2->PosY()){
+			puntaje += 100;
+			DisparoActivo = false;
+			gotoxy (27,10);
+			mensaje = rand()%3+1;
+			switch(mensaje){
+			case 1:
+				cout<<"Good shot!        ";
+				break;
+			case 2:
+				cout<<"Bullseye!         ";
+				break;
+			case 3:
+				cout<<"Amazing!         ";
+				break;
+				
+			};
+			gotoxy(12,18);
+			textcolor(7);
+			cout<<puntaje;
+		};
+		
+	};
+}
+void Sistema::IniciarJuego(){
+	DibujarPlantilla();
+	while (vidas > 0){
+		Logica();
+		Colisiones();
+		Teclado();
+		
+	}
+	gameover();
+}
+
+int main(int argc, char *argv[]) {
+	Sistema sistema;
+	sistema.IniciarJuego();
+		
 	
 	return 0;
 }
